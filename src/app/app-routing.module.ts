70d4b59311080c0e1aaefb8c9456a1e80bb469a7
Jakkawal23 <Jakkawal.phooluemsai@gmail.com>
2023-09-08 19:10:35 +0700

    create project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/admin/add-employee/add-employee.component';
+import { NotificationComponent } from './components/admin/notification/notification.component';
+import { ManageEmployeesComponent } from './components/admin/manage-employees/manage-employees.component';
+import { EmployeeProfileComponent } from './components/employee/employee-profile/employee-profile.component';
+import { EditEmployeeComponent } from './components/employee/edit-employee/edit-employee.component';
+import { ChangePasswordComponent } from './components/employee/change-password/change-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: AddEmployeeComponent
+  // },
+  {
+    path: 'addEmployee',
+    component: AddEmployeeComponent
+  },
+  {
+    path: 'notification',
+    component: NotificationComponent
+  },
+  {
+    path: 'manageEmployee',
+    component: ManageEmployeesComponent
+  },
+  {
+    path: 'employeeProfile',
+    component: EmployeeProfileComponent
+  },
+  {
+    path: 'editEmployee',
+    component: EditEmployeeComponent
+  },
+  {
+    path: 'changePassword',
+    component: ChangePasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddEmployeeComponent } from './components/admin/add-employee/add-employee.component';
+import { NotificationComponent } from './components/admin/notification/notification.component';
+import { ManageEmployeesComponent } from './components/admin/manage-employees/manage-employees.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { EmployeeProfileComponent } from './components/employee/employee-profile/employee-profile.component';
+import { EditEmployeeComponent } from './components/employee/edit-employee/edit-employee.component';
+import { ChangePasswordComponent } from './components/employee/change-password/change-password.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    NotificationComponent,
+    ManageEmployeesComponent,
+    EmployeeProfileComponent,
+    EditEmployeeComponent,
+    ChangePasswordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
